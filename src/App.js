8201e9c37d9917a7468bfe72ef2ebda9a2173e93
Marow Macaulay <Marow.Macaulay@contracted.pmi.com>
2022-12-25 21:41:38 +0100

    Initialized application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import CardComponent from "./components/CardComponent";
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState([
+    { id: 1, text: "Favorite" },
+    { id: 2, text: "Piechart" },
+    { id: 3, text: "Thumbup" },
+  ]);
+
+  const moveCard = (fromIndex, toIndex) => {
+    const updatedCards = [...cards];
+    const [removedCard] = updatedCards.splice(fromIndex, 1);
+    updatedCards.splice(toIndex, 0, removedCard);
+    setCards(updatedCards);
+  };
+
+  const handleDragOver = (e) => {
+    // Prevent default behavior (prevent file from being opened)
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    // Prevent default behavior (prevent file from being opened)
+    e.preventDefault();
+
+    // Get the card index and index that were transferred during the drag operation
+    const cardIndex = e.dataTransfer.getData("cardIndex");
+
+    // Find the index of the card being dropped
+    const dropIndex = e.target.getAttribute("data-index");
+
+    // Move the card to the new position
+    moveCard(cardIndex, dropIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: "#8f8f8fba",
+        margin: 0,
+        height: "100vh",
+        textAlign: "center",
+      }}
+    >
+      <h3
+        style={{
+          margin: 0,
+          padding: "20px",
+        }}
+      >
+        Story_Doc Assessment
+      </h3>
+      <div
+        className="app"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {cards.map((card, index) => (
+          <CardComponent
+            id={card.id}
+            text={card.text}
+            index={index}
+            key={card?.id}
+            className="card-container"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            data-index={index}
+            moveCard={moveCard}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,121 @@
+import { createElement, useRef, useState } from "react";
+import { Paper, Box, Fab } from "@mui/material";
+import {
+  Edit,
+  Favorite,
+  Cancel,
+  Remove,
+  Clear,
+  Print,
+  PieChart,
+  ThumbUp,
+} from "@material-ui/icons";
+import JsPDF from "jspdf";
+import html2canvas from "html2canvas";
+
+export default function CardComponent({ id, index, text, ...props }) {
+  const [icon, setIcon] = useState(text);
+  const [, setIsDragging] = useState(false);
+
+  const handleDragStart = (e) => {
+    // Set the data that will be transferred during the drag operation
+    e.dataTransfer.setData("cardId", id);
+    e.dataTransfer.setData("cardIndex", index);
+
+    // Add a custom dragging class to the element
+    e.target.classList.add("dragging");
+
+    setIsDragging(true);
+  };
+
+  const handleDragEnd = (e) => {
+    // Remove the dragging class from the element
+    e.target.classList.remove("dragging");
+
+    setIsDragging(false);
+  };
+
+  const generatePDF = () => {
+    const element = document.querySelector(`#card_${id}`);
+    const pdf = new JsPDF("p", "mm", "a4");
+
+    html2canvas(element, { scale: 2 })
+      .then((canvas) => {
+        pdf.addImage(canvas, "JPEG", 0, 0, 211, 298);
+      })
+      .then(() => {
+        pdf.save("section.pdf");
+      });
+  };
+
+  const spanRef = useRef(null);
+
+  function handleClick() {
+    spanRef.current.contentEditable = true;
+  }
+
+  function handleBlur() {
+    setIcon(spanRef.current.innerHTML);
+  }
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        "& > :not(style)": {
+          m: 1,
+          width: 170,
+          height: 230,
+        },
+      }}
+      id={`card_${id}`}
+    >
+      <Paper
+        {...props}
+        elevation={3}
+        sx={{
+          padding: "10px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Fab size="small" onClick={generatePDF} type="button">
+            <Print />
+          </Fab>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "0 0 20px",
+          }}
+          draggable
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          id="report"
+        >
+          {
+            {
+              Edit: createElement(Edit),
+              Favorite: createElement(Favorite),
+              Cancel: createElement(Cancel),
+              Remove: createElement(Remove),
+              Clear: createElement(Clear),
+              Piechart: createElement(PieChart),
+              Thumbup: createElement(ThumbUp),
+            }[icon]
+          }
+        </div>
+        <span ref={spanRef} onClick={handleClick} onInput={handleBlur}>
+          Insert text here
+        </span>
+        <p>Add here your additional text</p>
+      </Paper>
+    </Box>
+  );
+}
